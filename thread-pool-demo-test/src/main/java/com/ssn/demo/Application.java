27952e8b816cf8e73255d7642a055da07f2a3852
chengdonglin <chengdong2518@163.com>
2024-05-17 16:57:16 +0800

    feat: 测试工程搭建

---
+++ thread-pool-demo-test/src/main/java/com/ssn/demo/Application.java
@@ -0,0 +1,20 @@
+package com.ssn.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author linchengdong
+ * @Date 2024-05-17 14:11
+ * @PackageName:com.ssn.demo
+ * @ClassName: Application
+ * @Description: TODO
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ thread-pool-demo-test/src/main/java/com/ssn/demo/config/ThreadPoolConfig.java
@@ -0,0 +1,86 @@
+package com.ssn.demo.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.*;
+
+/**
+ * @Author linchengdong
+ * @Date 2024-05-17 14:14
+ * @PackageName:com.ssn.demo.config
+ * @ClassName: ThreadPoolConfig
+ * @Description: TODO
+ * @Version 1.0
+ */
+@Configuration
+@EnableConfigurationProperties(ThreadPoolConfigProperties.class)
+public class ThreadPoolConfig {
+
+    @Bean("threadPoolExecutor01")
+    public ThreadPoolExecutor threadPoolExecutor01(ThreadPoolConfigProperties properties) {
+        RejectedExecutionHandler handler;
+        switch (properties.getPolicy()){
+            case "AbortPolicy":
+                handler = new ThreadPoolExecutor.AbortPolicy();
+                break;
+            case "DiscardPolicy":
+                handler = new ThreadPoolExecutor.DiscardPolicy();
+                break;
+            case "DiscardOldestPolicy":
+                handler = new ThreadPoolExecutor.DiscardOldestPolicy();
+                break;
+            case "CallerRunsPolicy":
+                handler = new ThreadPoolExecutor.CallerRunsPolicy();
+                break;
+            default:
+                handler = new ThreadPoolExecutor.AbortPolicy();
+                break;
+        }
+
+        // 创建线程池
+        return new ThreadPoolExecutor(properties.getCorePoolSize(),
+                properties.getMaxPoolSize(),
+                properties.getKeepAliveTime(),
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(properties.getBlockQueueSize()),
+                Executors.defaultThreadFactory(),
+                handler);
+
+    }
+
+
+    @Bean("threadPoolExecutor02")
+    public ThreadPoolExecutor threadPoolExecutor02(ThreadPoolConfigProperties properties) {
+        // 实例化策略
+        RejectedExecutionHandler handler;
+        switch (properties.getPolicy()){
+            case "AbortPolicy":
+                handler = new ThreadPoolExecutor.AbortPolicy();
+                break;
+            case "DiscardPolicy":
+                handler = new ThreadPoolExecutor.DiscardPolicy();
+                break;
+            case "DiscardOldestPolicy":
+                handler = new ThreadPoolExecutor.DiscardOldestPolicy();
+                break;
+            case "CallerRunsPolicy":
+                handler = new ThreadPoolExecutor.CallerRunsPolicy();
+                break;
+            default:
+                handler = new ThreadPoolExecutor.AbortPolicy();
+                break;
+        }
+        // 创建线程池
+        return new ThreadPoolExecutor(properties.getCorePoolSize(),
+                properties.getMaxPoolSize(),
+                properties.getKeepAliveTime(),
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(properties.getBlockQueueSize()),
+                Executors.defaultThreadFactory(),
+                handler);
+    }
+
+
+}
